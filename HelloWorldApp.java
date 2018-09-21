4a638ae79a94941df1e89dfa14751e08a20b4efb
Tyson Beckham <njoecopp@gmail.com>
2018-09-21 15:24:09 -0400

    what is happening

---
+++ HelloWorldApp.java
@@ -0,0 +1,9 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("You are going to kill it this year!"); // Display the string.
+    }
+}
